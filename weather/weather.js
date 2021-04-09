414b42fde0558781e834ff2d05eff1888554df5a
hmiden1 <77756911+hmiden1@users.noreply.github.com>
2021-04-08 22:08:57 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,7 +1,6 @@
 var map = L.map('map').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
-
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
@@ -9,8 +8,16 @@ var radarDisplayOptions = {
   transparent: true
 };
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
-
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
- L.geoJSON(data).addTo(map);
-});
+L.geoJSON(data, {
+  style: function(feature){
+  var alertColor = 'orange';
+  if (feature.properties.severity === 'Severe') alertColor = 'red';
+  return { color: alertColor };
+},
+  onEachFeature: function(feature, layer) {
+  layer.bindPopup(feature.properties.headline);
+}
+}).addTo(map);
+
